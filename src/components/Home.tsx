d3fd38d03353c361d2d5f32603187e6a7296a0f0
alexis <alexisbrou1@hotmail.fr>
2021-09-06 11:28:20 -0400

    simplify map

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,40 +1,36 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {  GoogleApiWrapper } from 'google-maps-react';
 import './home.css';
 import { calculateAndDisplayRoute } from '../functions/mapFunctions';
+import MapsSearchBar from './MapsSearchBar';
 
 const Home =()=>{
     const mapRef: React.MutableRefObject<HTMLElement | null> = useRef(null); 
-    const [mapReady, setMapReady] = useState(false);
+    let map = null
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
 
+    useEffect(() => {
+      generateMap();
+    }, []);
+
+
   const generateMap = () => {
-    if (mapReady) {
-      const map = new google.maps.Map(
-        mapRef.current != null ? mapRef.current : new HTMLElement(),
-        {
-          //to be deleted, just to remember
-          zoom: 15,
-          center: { lat: -25.344, lng: 131.036 },
-        }
-      );
+      map = new google.maps.Map(
+        mapRef.current != null ? mapRef.current : new HTMLElement());
       directionsRenderer.setMap(map);
       calculateAndDisplayRoute(directionsService, directionsRenderer);
-    }
-    return null
-  }
-
-  const checkMapReady=(element: HTMLElement | null)=>{
-    mapRef.current = element;
-    if (element != null && !mapReady)
-      setMapReady(true);
   }
 
   return (
-    <div id="map" ref={element => {checkMapReady(element)}}>
-      {generateMap()}
+    <div>
+      {/* <MapsSearchBar map={map}/> */}
+        <div id="map" ref={element => {mapRef.current = element}}>
+
+        </div>
+        
     </div>
+    
 
   )
 }
